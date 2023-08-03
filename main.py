77258765a845dea10f9b6fa2a1be42bb1eed97df
ID058689 <iain.derrett@jacobs.com>
2023-08-03 14:14:03 +0100

    Create columns, image and description

---
+++ main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Iain Derrett")
+    content = """
+    Hi, I am Iain! I am a civil engineer who is starting a journey to self-learn the skills necessary to become a DevOps engineer.
+    """
+    st.info(content)
